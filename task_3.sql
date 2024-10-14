25160fd183eb7ca7f493342b9ebe9b33b08fb17d
khalid fathy <dent.khalidfathy@gmail.com>
2024-10-14 23:25:48 +0300

    Added task_3.sql script to list tables

---
+++ task_3.sql
@@ -0,0 +1,8 @@
+-- task_3.sql
+-- This script lists all tables in a given database.
+
+-- Replace "alx_book_store" with the actual database name.
+USE `alx_book_store`;
+
+-- List all tables in the database.
+SHOW TABLES;
